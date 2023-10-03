09f17d84a11c516360aa1135d9dca661f14b6af7
Justin <justin.warambourg@gmail.com>
2023-10-03 15:42:11 +0800

    fix: solve error when -W flags on

--- ft_libc/ft_isalnum.c
+++ ft_libc/ft_isalnum.c
@@ -1,3 +1,4 @@
-int ft_isalnum(int c){
-    return (47 < c && c < 58 || 64 < c && c < 91 || 96 < c && c < 123);
+int ft_isalnum(int c)
+{
+    return ((47 < c && c < 58) || (64 < c && c < 91) || (96 < c && c < 123));
 }
