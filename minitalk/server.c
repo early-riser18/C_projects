26ec27aae00cc8dcb0a66208f5b653b9e5bc9477
Justin <justin.warambourg@gmail.com>
2023-11-14 13:01:30 +0800

    chore: refactored for increased readability

--- minitalk/server.c
+++ minitalk/server.c
@@ -4,13 +4,13 @@
 
 char	g_bin_char = 0;
 
-void	handler_sigusr(int signum)
-{	
+void	add_next_bit(int signum)
+{
 	if (signum == SIGUSR1)
 	{
 		g_bin_char >>= 1;
 		g_bin_char &= 0x7F;
-	} 
+	}
 	else if (signum == SIGUSR2)
 	{
 		g_bin_char >>= 1;
@@ -28,52 +28,53 @@ char	*ft_realloc(char *s, size_t size)
 	return (temp);
 }
 
+void	handler_sigusr(int signum)
+{
+	static char	*output_msg = NULL;
+	static int	byte_i = 0;
+	const int	block_size = 10;
+
+	add_next_bit(signum);
+	byte_i++;
+	if (byte_i > 7)
+	{
+		if (!output_msg)
+		{
+			output_msg = ft_calloc(block_size, 1);
+			if (!output_msg)
+			{
+				printf("Malloc failed");
+				exit(1);
+			}
+		}
+		if ((ft_strlen(output_msg) + 1) % block_size == 0)
+		{
+			output_msg = ft_realloc(output_msg, ft_strlen(output_msg) + 1 + block_size);
+		}
+		output_msg[ft_strlen(output_msg)] = g_bin_char;
+		if (g_bin_char == 0)
+		{
+			printf("%s\n", output_msg);
+			free(output_msg);
+			output_msg = NULL;
+		}
+		byte_i = 0;
+		g_bin_char = 0;
+	}
+}
+
 int	main(void)
 {
-	char				*output_msg;
-	int					byte_i;
-	const int			block_size = 10;
 	struct sigaction	sa;
 
-	byte_i = 0;
-	g_bin_char = 0;
 	sa.sa_handler = handler_sigusr;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-
-	printf("%d\n", getpid());
-	output_msg = ft_calloc(block_size, 1);
-	if (!output_msg)
-	{
-		printf("Malloc failed");
-		exit(1);
-	}
+	printf("Server's PID: %d\n", getpid());
 	while (1)
-	{	
+	{
 		pause();
-		byte_i++;
-		if (byte_i > 7)
-		{
-			if (!output_msg)
-			{
-				output_msg = ft_calloc(block_size, 1);
-			}
-			if ((ft_strlen(output_msg) + 1) % block_size == 0)
-			{
-				output_msg = ft_realloc(output_msg, ft_strlen(output_msg) + 1 + block_size);
-			}
-			output_msg[ft_strlen(output_msg)] = g_bin_char;
-			if (g_bin_char == 0)
-			{
-				printf("%s\n", output_msg);
-				free(output_msg);
-				output_msg = NULL;
-			}
-			byte_i = 0;
-			g_bin_char = 0;
-		}
 	}
-	return (0);
 }
